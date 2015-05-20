6b61b6eca067777fb8b687603d57b3b77c119d62
MattiasLovstrom <123Kaffe>
2015-05-20 22:13:22 +0200

    Updated for NuGet

--- WebApiCache/Properties/AssemblyInfo.cs
+++ WebApiCache/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebApiCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Client/server cache solution for Microsoft WebAPI implementations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mattias Lövström")]
 [assembly: AssemblyProduct("WebApiCache")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.*")]
